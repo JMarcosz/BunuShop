f8d0ac08d45356ce827e5044838d321436b37391
Jean Marte <jeanmarte22@gmail.com>
2026-09-05 01:37:45 -0400

    feat: actualizar dominio a bunushop.store y metricas a 100 pedidos y 60 clientas

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://bunushop.store',
   output: 'server',
   adapter: vercel(),
   integrations: [tailwind({ applyBaseStyles: false })],
